a86105ec098c0f52f6ac0851f1a2d598115f3ac4
“evamariat” <eva.maria.truve@iCloud.com>
2026-03-08 13:55:28 +0200

    next config uncomment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig = {
     experimental: {
-    optimizeCss: false  // Disables aggressive CSS chunking
+    optimizeCss: false
   },
   images: {
     remotePatterns: [
@@ -12,10 +12,10 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
-      // Add your Supabase storage too
+
       {
         protocol: 'https',
-        hostname: '*.supabase.co',
+        hostname: 'https://lzcaafgvwqgiuubnaotu.supabase.co',
         port: '',
         pathname: '/storage/**',
       },
