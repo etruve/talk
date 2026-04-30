f885680f62d652a8a63767fba61d69b192a65925
“evamariat” <eva.maria.truve@iCloud.com>
2026-04-30 16:28:21 +0300

    Sound  cards.

--- src/components/video-covered-card.tsx
+++ src/components/video-covered-card.tsx
@@ -9,20 +9,19 @@ type Props = {
 }
 
 const supabase = createClient();
-
-// This synchronously generates the public URL for the video
-const { data:soundData } = supabase.storage
-  .from("cat")
-  .getPublicUrl("Moment.mp4");
-
-const soundUrl = soundData.publicUrl;
-
 // This synchronously generates the public URL for the video
 const { data:sound1Data } = supabase.storage
   .from("cat")
   .getPublicUrl("Roots.mp3");
 
 const sound1Url = sound1Data.publicUrl;
+
+// This synchronously generates the public URL for the video
+const { data:sound2Data } = supabase.storage
+  .from("cat")
+  .getPublicUrl("Right.mp3");
+
+const sound2Url = sound2Data.publicUrl;
 
 const { data: otherData } = supabase.storage
   .from("cat")
@@ -49,6 +48,16 @@ return (
         onClick={() => setIsRevealed(!isRevealed)}
       >
         <CardContent className="p-0">
+         <audio
+            className="w-full h-16 object-cover rounded-lg"
+            controls
+            src={sound1Url}
+          />    
+         <audio
+            className="w-full h-16 object-cover rounded-lg"
+            controls
+            src={sound2Url}
+          />    
         <video
             className="w-full h-64 object-cover rounded-lg"
             controls
