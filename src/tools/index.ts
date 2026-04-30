7bd2da45a76aa4443b2370d811b170fd32e6eb36
“evamariat” <eva.maria.truve@iCloud.com>
2026-04-30 10:24:04 +0300

    cleanup

--- src/tools/index.ts
+++
@@ -1,9 +0,0 @@
-import { writeEnvVar } from "./writeEnvVar.js";
-import { createRlsPolicy } from "./createRlsPolicy.js";
-import { applyMigration } from "./applyMigration.js";
-
-export const tools = [
-  writeEnvVar,
-  createRlsPolicy,
-  applyMigration
-];
